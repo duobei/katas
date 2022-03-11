1c1e63975bdca808487b802b653f61b79be8785d
duobei <duobei.zh@gmail.com>
2022-03-11 09:40:41 +0800

    add the test case for the number which is indivisible by 3 or 5

--- fizzbuzz/test/fizzbuzz_test.exs
+++ fizzbuzz/test/fizzbuzz_test.exs
@@ -2,7 +2,9 @@ defmodule FizzbuzzTest do
   use ExUnit.Case
   doctest Fizzbuzz
 
-  test "greets the world" do
-    assert Fizzbuzz.hello() == :world
+  test "return the number when it is indivisible by 3 or 5" do
+    assert Fizzbuzz.fizzbuzz(1) == 1
+    assert Fizzbuzz.fizzbuzz(2) == 2
+    assert Fizzbuzz.fizzbuzz(4) == 4
   end
 end
