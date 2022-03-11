2095a7cd910a1244008076c340870e938c2bcd9c
duobei <duobei.zh@gmail.com>
2022-03-11 16:02:33 +0800

    implement part1

--- toboggan_travel/lib/toboggan_travel.ex
+++ toboggan_travel/lib/toboggan_travel.ex
@@ -3,16 +3,45 @@ defmodule TobogganTravel do
   Documentation for `TobogganTravel`.
   """
 
-  @doc """
-  Hello world.
+  def part1(input), do: count_trees(input, {3, 1})
 
-  ## Examples
+  def count_trees(input, {dx, dy}) do
+    map = build_map(input)
 
-      iex> TobogganTravel.hello()
-      :world
+    Stream.iterate({0, 0}, fn {x, y} ->
+      {x + dx, y + dy}
+    end)
+    |> Enum.reduce_while(0, fn position, total ->
+      case see(map, position) do
+        :tree ->
+          {:cont, total + 1}
 
-  """
-  def hello do
-    :world
+        :square ->
+          {:cont, total}
+
+        nil ->
+          {:halt, total}
+      end
+    end)
   end
+
+  defp build_map(input) do
+    lines = String.split(input, "\n", trim: true)
+    height = length(lines)
+    width = lines |> hd() |> String.length()
+
+    {width, height, lines}
+  end
+
+  defp see({width, height, lines}, {x, y}) when y < height do
+    lines
+    |> Enum.at(y)
+    |> String.at(rem(x, width))
+    |> case do
+      "#" -> :tree
+      "." -> :square
+    end
+  end
+
+  defp see(_, _), do: nil
 end
