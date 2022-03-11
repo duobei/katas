4ecc315b0d202c6e7337e478d305323924766325
duobei <duobei.zh@gmail.com>
2022-03-11 15:28:57 +0800

    add test case for part 1 example

--- toboggan_travel/test/toboggan_travel_test.exs
+++ toboggan_travel/test/toboggan_travel_test.exs
@@ -2,7 +2,25 @@ defmodule TobogganTravelTest do
   use ExUnit.Case
   doctest TobogganTravel
 
-  test "greets the world" do
-    assert TobogganTravel.hello() == :world
+  describe "part 1" do
+    test "for example" do
+      assert TobogganTravel.part1(example()) == 7
+    end
+  end
+
+  defp example() do
+    """
+    ..##.......
+    #...#...#..
+    .#....#..#.
+    ..#.#...#.#
+    .#...##..#.
+    ..#.##.....
+    .#.#.#....#
+    .#........#
+    #.##...#...
+    #...##....#
+    .#..#...#.#
+    """
   end
 end
