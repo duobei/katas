466111d90447ec4259403f88717176148c7aaf65
duobei <duobei.zh@gmail.com>
2022-03-11 10:30:44 +0800

    add test case for list_fizzbuzz when parameter is not integer

--- fizzbuzz/test/fizzbuzz_test.exs
+++ fizzbuzz/test/fizzbuzz_test.exs
@@ -31,22 +31,28 @@ defmodule FizzbuzzTest do
   describe "list_fizzbuzz/1" do
     test "return the fizzbuzz list below the num" do
       assert Fizzbuzz.list_fizzbuzz(15) == [
-        "1",
-        "2",
-        "fizz",
-        "4",
-        "buzz",
-        "fizz",
-        "7",
-        "8",
-        "fizz",
-        "buzz",
-        "11",
-        "fizz",
-        "13",
-        "14",
-        "fizz buzz"
-      ]
+               "1",
+               "2",
+               "fizz",
+               "4",
+               "buzz",
+               "fizz",
+               "7",
+               "8",
+               "fizz",
+               "buzz",
+               "11",
+               "fizz",
+               "13",
+               "14",
+               "fizz buzz"
+             ]
+    end
+
+    test "n is not integer" do
+      assert_raise RuntimeError, "invalid parameter", fn ->
+        Fizzbuzz.list_fizzbuzz("15")
+      end
     end
   end
 end
