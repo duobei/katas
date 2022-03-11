03c64ce25de72d839e0856fb516084c5bd7b7157
duobei <duobei.zh@gmail.com>
2022-03-11 09:47:13 +0800

    add test case for the number is divisible by 3

--- fizzbuzz/test/fizzbuzz_test.exs
+++ fizzbuzz/test/fizzbuzz_test.exs
@@ -7,4 +7,10 @@ defmodule FizzbuzzTest do
     assert Fizzbuzz.fizzbuzz(2) == 2
     assert Fizzbuzz.fizzbuzz(4) == 4
   end
+
+  test "return fizz when the number is divisible by 3" do
+    assert Fizzbuzz.fizzbuzz(3) == "fizz"
+    assert Fizzbuzz.fizzbuzz(6) == "fizz"
+    assert Fizzbuzz.fizzbuzz(9) == "fizz"
+  end
 end
